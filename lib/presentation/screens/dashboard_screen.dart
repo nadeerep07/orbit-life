79965d86558dd5080315b7ea06bd4068ecbd0c17
NADEER E P <nadeerep07@gmail.com>
2026-08-19 22:13:48 +0530

    fix(dashboard): remove dead imports on main dashboard

--- lib/presentation/screens/dashboard_screen.dart
+++ lib/presentation/screens/dashboard_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import '../../core/utils/app_routes.dart';
 import '../../core/utils/responsive.dart';
 import '../viewmodels/accounts_view_model.dart';
