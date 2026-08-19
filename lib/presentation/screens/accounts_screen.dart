c583c5b987d99843187e49457be41237278e3b72
NADEER E P <nadeerep07@gmail.com>
2026-08-19 22:13:17 +0530

    fix(accounts_screen): clean up unused screen dependencies

--- lib/presentation/screens/accounts_screen.dart
+++ lib/presentation/screens/accounts_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../theme/app_theme.dart';
 import '../viewmodels/accounts_view_model.dart';
 import '../../core/utils/currency_formatter.dart';
 import '../../core/utils/responsive.dart';
